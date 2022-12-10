dfe57db30187653d7dbe09364bd14e89eaa34513
Promise-Devotion <sijun0224@msn.com>
2022-12-10 23:48:27 +0800

    配置eslint

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 
-import "./index.css";
+import './index.css'
 
-const div = document.getElementById("root") as HTMLElement;
-const root = ReactDOM.createRoot(div);
+const div = document.getElementById('root') as HTMLElement
+const root = ReactDOM.createRoot(div)
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
